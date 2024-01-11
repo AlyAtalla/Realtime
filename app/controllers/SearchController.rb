b938a8979a792a47553210705a9599dd14b0731d
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2024-01-11 16:53:14 +0200

    fix linter errors

--- app/controllers/SearchController.rb
+++ app/controllers/SearchController.rb
@@ -1,10 +1,9 @@
 class SearchController < ApplicationController
   def index
-    if params[:query].present?
-      @search_results = Article.where("title LIKE ?", "%#{params[:query]}%")
-    else
-      @search_results = Article.all
-    end
+    @search_results = if params[:query].present?
+                        Article.where('title LIKE ?', "%#{params[:query]}%")
+                      else
+                        Article.all
+                      end
   end
- end
- 
+end
