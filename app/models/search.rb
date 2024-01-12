6974fa48c39bbc05783f29dc5b55c02f5bfcbf86
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2024-01-12 13:30:02 +0200

    update views

--- app/models/search.rb
+++
@@ -1,9 +0,0 @@
-class Search < ApplicationRecord
-  validates :query, presence: true
-
-  def self.record_search(query, user_ip)
-    return if query.blank?
-
-    create(query:, user_ip:)
-  end
-end
